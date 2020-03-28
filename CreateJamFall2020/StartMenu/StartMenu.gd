c123f470a53cd3c5d56158f6293867d1ab35ff55
Emil Valentin <atomicpotato8@gmail.com>
2020-03-28 17:33:07 +0100

    Finished Start Menu
    Finished the start menu scene

--- CreateJamFall2020/StartMenu/StartMenu.gd
+++ CreateJamFall2020/StartMenu/StartMenu.gd
@@ -3,11 +3,21 @@ extends Control
 
 func _ready():
 	$AudioStreamPlayer.play()
+	$CreditScreen.hide()
+	$Back.hide()
 	pass
 
 func _on_Play_pressed():
-	pass # Replace with function body.
+	pass 
 
 
 func _on_Credits_pressed():
-	pass # Replace with function body.
+	$CreditScreen.show()
+	$Back.show()
+	pass 
+
+
+func _on_Back_pressed():
+	$CreditScreen.hide()
+	$Back.hide()
+	pass 
