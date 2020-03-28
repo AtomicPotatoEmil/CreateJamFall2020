19702fc798680fbd50dd23ce1e31403dd8c56224
Emil Valentin <atomicpotato8@gmail.com>
2020-03-28 15:18:31 +0100

    Start Menu has been added to project
    Made a Start Menu screen which now has intractable buttons, and music playing in the background

---
+++ CreateJamFall2020/StartMenu/StartMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready():
+	$AudioStreamPlayer.play()
+	pass
+
+func _on_Play_pressed():
+	pass # Replace with function body.
+
+
+func _on_Credits_pressed():
+	pass # Replace with function body.
