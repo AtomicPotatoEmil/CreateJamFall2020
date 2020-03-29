25fcd1b1f8d0f26956ca8ec5d8c6febf662f2e3a
Sebastian <sebber280@live.dk>
2020-03-29 06:07:38 +0200

    Added more sprites and dialogue

--- CreateJamFall2020/GameState/Dialogue.gd
+++ CreateJamFall2020/GameState/Dialogue.gd
@@ -4,32 +4,43 @@ var RedLady_Dialogue = {
 	"greetings": "Hello handsome <3",
 	"Smalltalk": "You know, a flower is the most delicate thing a girl can wish for, but a drink would do just as fine",
 	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
-	"skeleton": "GOOOODBYE PERIODS! WOOOHOOO!"
+	"skeleton": "GOOOODBYE PERIODS! WOOOHOOO!",
+	"fat": "Oh boy I feel bloated...  I wouldn’t mind a snack though",
+	"thin": "Oh yeah! Skinny as a supermodel",
+	"hot": "I’m a perfect 5/7!"
 }
 
 var Heiress_Dialogue = {
 	"greetings": "Greetings sir...",
 	"Smalltalk": "Momma always used to say “a glass of warm milk with cinnamon will shut those eyes right up!”.... I stuck with wine though…",
 	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
-	"skeleton": "GOOOODBYE PERIODS! WOOOHOOO!"
+	"skeleton": "GOOOODBYE PERIODS! WOOOHOOO!",
+	"fat": "Oh boy I feel bloated...  I wouldn’t mind a snack though",
+	"thin": "Oh yeah! Skinny as a supermodel",
+	"hot": "I’m a perfect 5/7!"
 }
 
 var Singer_Dialogue = {
 	"greetings": "Oh, baby, when you talk like that, you make a woman go mad...",
 	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
 	"skeleton": "GOOOODBYE PERIODS! WOOOHOOO!",
-	"fat": "Oh boy I feel bloated...  I wouldn’t mind a snack though"
+	"fat": "Oh boy I feel bloated...  I wouldn’t mind a snack though",
+	"thin": "Oh yeah! Skinny as a supermodel",
+	"hot": "I’m a perfect 5/7!"
 }
 
 var DarkBowlerhatGuy_Dialogue = {
 	"greetings": "Hey, what do you want?",
 	"Smalltalk": "Why is there never any whisky in this bar?... I swear every time I ask, they’re out!",
 	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
-	"skeleton": "“I wonder if I can still pee…. God i’m gonna miss masturbating"
+	"skeleton": "I wonder if I can still pee…. God i’m gonna miss masturbating",
+	"fat": "Oh boy I feel bloated...  I wouldn’t mind a snack though",
+	"thin": "Oh yeah! Skinny as a supermodel",
+	"hot": "I’m a perfect 5/7!"
 }
 
 var LightBowlerhatGuy_Dialogue = {
 	"greetings": "Wow man look at us... we’re almost identical!",
 	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
-	"skeleton": "“I wonder if I can still pee…. God i’m gonna miss masturbating"
+	"skeleton": "I wonder if I can still pee…. God i’m gonna miss masturbating"
 }
