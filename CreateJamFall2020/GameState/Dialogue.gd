46863881d987a36be873bec4e2bf246c74213ac6
Sebastian <sebber280@live.dk>
2020-03-29 05:44:11 +0200

    Updated dialogue

--- CreateJamFall2020/GameState/Dialogue.gd
+++ CreateJamFall2020/GameState/Dialogue.gd
@@ -2,7 +2,34 @@ extends Node
 
 var RedLady_Dialogue = {
 	"greetings": "Hello handsome <3",
-	"Smalltalk": "",
-	"zombie": ""
-	
+	"Smalltalk": "You know, a flower is the most delicate thing a girl can wish for, but a drink would do just as fine",
+	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
+	"skeleton": "GOOOODBYE PERIODS! WOOOHOOO!"
+}
+
+var Heiress_Dialogue = {
+	"greetings": "Greetings sir...",
+	"Smalltalk": "Momma always used to say “a glass of warm milk with cinnamon will shut those eyes right up!”.... I stuck with wine though…",
+	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
+	"skeleton": "GOOOODBYE PERIODS! WOOOHOOO!"
+}
+
+var Singer_Dialogue = {
+	"greetings": "Oh, baby, when you talk like that, you make a woman go mad...",
+	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
+	"skeleton": "GOOOODBYE PERIODS! WOOOHOOO!",
+	"fat": "Oh boy I feel bloated...  I wouldn’t mind a snack though"
+}
+
+var DarkBowlerhatGuy_Dialogue = {
+	"greetings": "Hey, what do you want?",
+	"Smalltalk": "Why is there never any whisky in this bar?... I swear every time I ask, they’re out!",
+	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
+	"skeleton": "“I wonder if I can still pee…. God i’m gonna miss masturbating"
+}
+
+var LightBowlerhatGuy_Dialogue = {
+	"greetings": "Wow man look at us... we’re almost identical!",
+	"zombie": "Most zombies prefer brains, but i’m more of a feet-fan so to speak",
+	"skeleton": "“I wonder if I can still pee…. God i’m gonna miss masturbating"
 }
